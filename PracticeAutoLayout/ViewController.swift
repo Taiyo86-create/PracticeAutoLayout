b1716c5577badba6a38f7cbe43cad1869b1d0ea9
Taiyo Mzumoto <154989695+Taiyo86-create@users.noreply.github.com>
2024-09-05 09:34:55 +0900

    Initial commit

---
+++ PracticeAutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeAutoLayout
+//
+//  Created by t_mizumoto on 2024/09/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
